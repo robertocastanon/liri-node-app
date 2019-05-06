d96271ef6762752a02075deb7908dfc76158cdc3
Roberto Castanon <robertocastanon925@gmail.com>
2019-05-05 22:54:09 -0700

    cleaned up the code and made it more readable

--- liri.js
+++ liri.js
@@ -1,143 +1,127 @@
-// add code to read & set any environment var with dotenv package
-require("dotenv").config();
-// import the keys.js file then store it into a var
-var keys = require("./keys.js");
-//require axios for the omdb and bandsintown api
-var axios = require('axios')
-//need for the npm spotify api to work
-var Spotify = require('node-spotify-api')
-//will be used for the do-what-it-says case
-var fs = require('fs')
-var input = process.argv
-//will process the users first input after the filename
-var search = input[2];
-//in case your response has multiple words
-var term = input.slice(3).join(" ");
+/* ==================== INITIAL VARIABLES ==================== */
 
-//switch case for the four possible choices
-switch(search) {
+require("dotenv").config(); // add code to read & set any environment var with dotenv package
+var keys = require("./keys.js"); // import the keys.js file then store it into a var
+var axios = require('axios'); // require axios for the omdb and bandsintown api
+var Spotify = require('node-spotify-api'); // need for the npm spotify api to work
+var fs = require('fs'); // will be used for the do-what-it-says case
+var input = process.argv // var to reuse in the next two vars
+var search = input[2]; // will process the users first input after the filename
+var term = input.slice(3).join(" "); // in case your response has multiple words
+
+/* ==================== SWITCH CASE ==================== */
+
+switch(search) { // switch case for the 4 possible choices
     case 'concert-this':
-    if(!term) {
+    if(!term) { // if no term is given then it will default to "Earl Sweatshirt"
         term = "Earl Sweatshirt"
     }
     console.log('*Loading concerts near you*')
     console.log("-------------------------------------")
-    concert(term)
+    concert(term) // calls the concert function
     break;
     case 'spotify-this-song':
-    if(!term) {
+    if(!term) { //if no term is given then it will default to "The Sign by Ace of Base"
         term = "The Sign Ace of Base"
     }
     console.log('*Loading song info*')
     console.log("-------------------------------------")
-    spotify(term);
+    spotify(term); // calls the spotify function
     break;
     case 'movie-this':
-    if(!term) {
+    if(!term) { // if no term is given then it will default to "Mr. Nobody"
         term = "Mr. Nobody"
     }
     console.log('*Loading movie info*')
     console.log("-------------------------------------")
-    movie(term)
+    movie(term) // calls the movie function
     break;
     case 'do-what-it-says':
-    random()
+    random() // calls the random function
 }
-//function for bandsintown api and concert search
-function concert(term) {
-    var URL = `https://rest.bandsintown.com/artists/${term}/events?app_id=codingbootcamp`;
 
+/* ==================== FUNCTIONS ==================== */
+
+function concert(term) { // function for concert-this command
+    var URL = `https://rest.bandsintown.com/artists/${term}/events?app_id=codingbootcamp`; // variable to store the bandsintown api url
     axios.get(URL).then(function (response) {
-        // will display information about the first 5 venues
-        for (i = 0; i<5; i++) {
-        var jsonData = response.data[i]
-        var concertDisplay = [
-        "Venue Name: " + jsonData.venue.name,
-        "Venue Location: " + jsonData.venue.city + ", " + jsonData.venue.region,
-        "Date of Event: " + jsonData.datetime,
-        "-------------------------------------"
+        for (i = 0; i<5; i++) { // will display information about the first 5 venues
+        var jsonData = response.data[i] // stores the json data in a variable times 5
+        var concertDisplay = [ // variable to store concert info
+        `Venue Name: ${jsonData.venue.name}`,
+        `Venue Location: ${jsonData.venue.city}, ${jsonData.venue.region}`,
+        `Date of Event: ${jsonData.datetime}`,
+        `-------------------------------------`
         ].join('\n\n')
-        console.log(concertDisplay);
-
-        fs.appendFile('log.txt', concertDisplay, function (err) {
+        console.log(concertDisplay); // display information
+        fs.appendFile('log.txt', concertDisplay, function (err) { // append/send data to log.txt
             if (err) throw err
         })
-
         }
     })
 }
 
-// function for the spotify call, taken from documentation
-function spotify(term) {
-
+function spotify(term) { // function for spotify-this command
     var spotify = new Spotify(keys.spotify);
-
-    spotify.search({ type: 'track', query: term }, function(err,data) {
+    spotify.search({ type: 'track', query: term }, function(err,data) { // parameters for spotify npm
         if (err) {
             console.log('Error occurred: ' + err);
             return
         }
-        //parse the data given by the api
-        jsonData = data.tracks.items
-        var spotifyDisplay = [
-        //displays the information using the parsed jsonData
-        "Artist(s): " + jsonData[0].artists[0].name,
-        "Song Name: " + jsonData[0].name,
-        "Preview Link: " + jsonData[0].preview_url,
-        "Album: " + jsonData[0].album.name,
-        "-------------------------------------"
+        jsonData = data.tracks.items //store the json data in a variable and parse it
+        var spotifyDisplay = [ // variable to store and spotify info
+        `Artist(s): ${jsonData[0].artists[0].name}`,
+        `Song Name: ${jsonData[0].name}`,
+        `Preview Link: ${jsonData[0].preview_url}`,
+        `Album: ${jsonData[0].album.name}`,
+        `-------------------------------------`
         ].join('\n\n')
-
-        fs.appendFile('log.txt', spotifyDisplay, function (err) {
+        fs.appendFile('log.txt', spotifyDisplay, function (err) { // append/send data to log.txt
             if (err) throw err
-            console.log(spotifyDisplay);
+            console.log(spotifyDisplay); // display information
         })
     });
 }
 
-//function for calling the omdb api
-function movie(term) {
-    var URL = `http://www.omdbapi.com/?t=${term}&y=&plot=short&apikey=trilogy`;
-
+function movie(term) { //function for movie-this command
+    var URL = `http://www.omdbapi.com/?t=${term}&y=&plot=short&apikey=trilogy`; // variable to store omdb api url
     axios.get(URL).then(function (response) {
         //store json data in var
-        var jsonData = response.data
-        var movieDisplay = [
-        "Title: " + jsonData.Title,
-        "Release Date: " + jsonData.Released,
-        "IMDB Rating: " + jsonData.imdbRating,
-        "Rotten Tomatoe Rating: " + jsonData.Ratings[1].Value,
-        "Country: " + jsonData.Country,
-        "Language: " + jsonData.Language,
-        "Plot: " + jsonData.Plot,
-        "-------------------------------------"
+        var jsonData = response.data // store the json data in a variable
+        var movieDisplay = [ // variable to store the movie info
+        `Title: ${jsonData.Title}`,
+        `Release Date: ${jsonData.Released}`,
+        `IMDB Rating: ${jsonData.imdbRating}`,
+        `Rotten Tomatoe Rating: ${jsonData.Ratings[1].Value}`,
+        `Country: ${jsonData.Country}`,
+        `Language: ${jsonData.Language}`,
+        `Plot: ${jsonData.Plot}`,
+        `-------------------------------------`
         ].join('\n\n')
-        // will append data from movie and then display it to user
-        fs.appendFile('log.txt', movieDisplay, function (err) {
+        fs.appendFile('log.txt', movieDisplay, function (err) { // append/send data to log.txt
             if (err) throw err
-            console.log(movieDisplay);
+            console.log(movieDisplay); // display information
         })
     })
 
 }
 
-function random() {
-    fs.readFile('random.txt', 'utf8', function(err, data) {
-
+function random() { // function for do-what-it-says command
+    fs.readFile('random.txt', 'utf8', function(err, data) { // reads the random.txt file and sets it as 'data'
         if(err) {
             return console.log(err);
         }
-        var dataArr = data.split(',');
-        var randomDisplay = [
-        "Running 'spotify-this-song' for 'I Want it That Way'",
-        "-------------------------------------",
-        spotify(dataArr[1])
+        var dataArr = data.split(','); // grabs data except for comma
+        var randomDisplay = [ // variable to store info for later usage
+        `Running 'spotify-this-song' for 'I Want it That Way'`,
+        `-------------------------------------`,
+        spotify(dataArr[1]) // calls the spotify-this function for 'I Want it That Way' text
         ].join('\n\n')
-
-        fs.appendFile('log.txt', randomDisplay, function (err) {
+        fs.appendFile('log.txt', randomDisplay, function (err) { // append/sends data to log.txt
             if (err) throw err
-            console.log(randomDisplay);
+            console.log(randomDisplay); // disaplay information
         })
-
     })
 }
+
+/* ==================== END ==================== */
